86ae1c1ac967fe722ad6eb52f37b604aa814c43d
Comamoca <comamoca.dev@gmail.com>
2023-02-04 07:42:49 +0900

    :tada: (install.ts) add boost's installer

--- install.ts
+++ install.ts
@@ -3,7 +3,7 @@ import $ from "https://deno.land/x/dax@0.24.1/mod.ts";
 try {
   await $`git clone https://github.com/Comamoca/boost`;
   await $`cd ./boost/`;
-  await $`deno install -f -A boost.ts`;
+  await $`deno install -f --allow-read --allow-write --allow-env --allow-run --allow-net boost.ts`;
   await $`cd ..`;
 } finally {
   await Deno.remove("./boost/", { recursive: true });
